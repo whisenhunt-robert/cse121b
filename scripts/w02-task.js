59d39aa3e26c53c29a78c40d4749e3e5bba80125
Robert Whisenhunt <dahweirdacus@gmail.com>
2023-09-22 15:07:15 -0400

    Work in progress but getting there.

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,21 +3,20 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+const fullName = "Robert Whisenhunt";
+let currentYear = Date(year);
+let profilePicture = "images/robert.png";
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.getElementById("robert.png");
 
 /* Step 4 - Adding Content */
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+currentYear.textContent = text;
+imageElement.setAttribute("src", profilePicture);
 
 
 /* Step 5 - Array */
